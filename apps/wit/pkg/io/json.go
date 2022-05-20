134d5d04d4069ea3feafb62bfb63fd348ea3e105
TheDonDope <thedondope@hey.com>
2022-05-21 00:53:43 +0200

    feat(pkg-io): add json writer and reader

--- apps/wit/pkg/io/json.go
+++ apps/wit/pkg/io/json.go
@@ -3,31 +3,64 @@ package io
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"wit/apps/wit/pkg/tracker"
 )
 
-// NewJSONPersistor ...
-func NewJSONPersistor(d Destination) Persistor {
-	return d
-}
-
-// NewJSONSelector ...
-func NewJSONSelector(s Source) Selector {
-	return s
-}
-
 // Commit ...
 func (d Destination) Commit(stash *tracker.Stash) {
-	fmt.Printf("Persisting stash: %v at destination: %v", stash, d)
+	fmt.Println(fmt.Printf("Destination.Commit(d: %v, stash: %v)", d, stash))
+
 	b, err := json.Marshal(stash)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("bytes: %v", b)
+	fmt.Println(fmt.Printf("  b: %v", b))
+
+	if _, err := os.Stat(string(d)); err == nil {
+		os.Remove(string(d))
+	}
+
+	f, err := os.OpenFile(string(d), os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fmt.Println(fmt.Printf("  f: %v", f))
+
+	f.Write(b)
 }
 
 // Pull ...
 func (s Source) Pull(name string) *tracker.Stash {
-	fmt.Printf("Pulling stash: %v from source: %v", name, s)
+	fmt.Println(fmt.Printf("Source.Pull(s: %v, name: %v)", s, name))
+
+	f, err := os.Open(string(s))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	fmt.Println(fmt.Printf("  f: %v", f))
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fmt.Printf("  b: %v", b))
+
+	var stashes []*tracker.Stash
+	json.Unmarshal(b, &stashes)
+	fmt.Println(fmt.Printf("  stashes:  %v", stashes))
+
+	for _, s := range stashes {
+		fmt.Println(fmt.Printf("  s: %v", s))
+		if s.Strain == name {
+			fmt.Println("found")
+			return s
+		}
+	}
+
+	fmt.Println("not found")
 	return &tracker.Stash{}
 }
