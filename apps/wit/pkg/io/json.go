749e9e9972e39b8f8426a805cda1daf8c72d20b2
TheDonDope <thedondope@hey.com>
2022-05-21 14:15:45 +0200

    refactor: overhaul interfaces and json implementation

--- apps/wit/pkg/io/json.go
+++ apps/wit/pkg/io/json.go
@@ -5,14 +5,18 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"os"
 	"wit/apps/wit/pkg/tracker"
 )
 
-// JSONSelector ...
+// JSONSelector acts as a type holder to implement the Selector interface.
 type JSONSelector struct{}
 
-// JSONPersistor ...
+// JSONPersistor acts as a type holder to implement the Persitor interface.
 type JSONPersistor struct{}
+
+// JSONRepository acts as a type holder to implement the Puller and Pusher interfaces.
+type JSONRepository struct{}
 
 // Checkout reads from the given io.Reader assuming JSON bytes
 // and returning an unmarshaled Stash instance.
@@ -37,6 +41,7 @@ func (j *JSONSelector) Checkout(r io.Reader) *tracker.Stash {
 // Commit writes to the given io.Writer
 func (j *JSONPersistor) Commit(w io.Writer, s ...*tracker.Stash) {
 	fmt.Println(fmt.Printf("JSONPersistor.Commit(w: %v, s: %v)", w, s))
+
 	for _, v := range s {
 		b, err := json.Marshal(v)
 		fmt.Println(fmt.Printf("  json.Marshal(): %v", b))
@@ -47,5 +52,40 @@ func (j *JSONPersistor) Commit(w io.Writer, s ...*tracker.Stash) {
 		if _, err := w.Write(b); err != nil {
 			panic(err)
 		}
+	}
+}
+
+// Pull checks for all existing (in-memory) stashes their corresponding
+// latest persistant state and updates accordingly.
+func (j *JSONRepository) Pull(r *Repository) {
+	fmt.Println(fmt.Printf("JSONRepository.Pull(r: %v)", r))
+
+	for _, v := range r.Stashes {
+		f, err := os.Open(v.Strain + ".json")
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		stash := r.Selector.Checkout(f)
+		r.Stashes[v.Strain] = stash
+	}
+}
+
+// Push persists all existing (in-memory) stashes to their corresponding
+// json files.
+func (j *JSONRepository) Push(r *Repository) {
+	fmt.Println(fmt.Printf("JSONRepository.Push(r: %v)", r))
+	for _, v := range r.Stashes {
+		path := v.Strain + ".json"
+		if _, err := os.Stat(path); err == nil {
+			os.Remove(path)
+		}
+		f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0644)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		r.Persistor.Commit(f)
 	}
 }
