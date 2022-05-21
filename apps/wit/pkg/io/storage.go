7fcc2a09e2873da847ddedf0e5ddfaed6b8b515a
TheDonDope <thedondope@hey.com>
2022-05-21 13:07:38 +0200

    feat: refactor repository and stash interfaces

--- apps/wit/pkg/io/storage.go
+++ apps/wit/pkg/io/storage.go
@@ -1,29 +1,35 @@
 package io
 
 import (
+	"io"
 	"wit/apps/wit/pkg/tracker"
 )
-
-// Source ...
-type Source string
-
-// Destination ...
-type Destination string
 
 // Persistor provides a way to save the state of a repository to disk.
 type Persistor interface {
 	// Persists the given stash to disk.
-	Commit(stash *tracker.Stash)
+	Commit(w io.Writer, s ...*tracker.Stash)
 }
 
 // Selector retrieves the stash with the given name from disk.
 type Selector interface {
 	// Retrieve the stash with the given name from disk.
-	Pull(name string) *tracker.Stash
+	Pull(r io.Reader) *tracker.Stash
 }
 
 // Repository holds the information about all the stashes.
 type Repository struct {
 	// All stashes as mapping of their name to their instance
+	r       io.Reader
+	w       io.Writer
 	Stashes map[string]*tracker.Stash
+}
+
+// NewRepository ...
+func NewRepository(r io.Reader, w io.Writer) *Repository {
+	return &Repository{
+		r:       r,
+		w:       w,
+		Stashes: make(map[string]*tracker.Stash),
+	}
 }
